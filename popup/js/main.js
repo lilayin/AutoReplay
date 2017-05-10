669683930567f177c3959aa8eb5d852b451eb7d8
lilayin <283789106@qq.com>
2017-05-10 22:20:30 +0800

    gogo

--- popup/js/main.js
+++ popup/js/main.js
@@ -48,7 +48,43 @@
 		localStorage.clear();
 	}
 	
-	
+	function MyAjax(url, method, success, failure) {
+		var request = new XMLHttpRequest();
+		request.onreadystatechange = function () {
+			var DONE = this.DONE || 4;
+			if (this.readyState === DONE){
+				success(this.responseText);
+			}else {
+				if(failure) {
+					failure();
+				}
+			}
+		};
+		request.open(method, url, false);
+		request.setRequestHeader('X-Requested-With', 'XMLHttpRequest'); 
+		request.send(null);
+
+	}
+
+
+	function genJin(id, content){
+		
+		var url = "http://crm.meierbei.com/Customer/CustomerGenJin";
+		var postData = "clientId="+id+"&content="+content;
+		var msg = "处理失败";
+		var request = new XMLHttpRequest();
+		request.onreadystatechange = function () {
+			var DONE = this.DONE || 4;
+			if (this.readyState === DONE){
+				msg = this.responseText;
+			}
+		};
+		request.open("POST", url, false);
+		request.setRequestHeader("X-Requested-With", "XMLHttpRequest"); 
+		request.send(postData);
+		return msg;
+	}	
+
 	$( "#tabs" ).tabs();
 	
 	
@@ -77,23 +113,56 @@
 			urls.push(urlTypeC);
 			urls.push(urlTypeD);
 
-			for(var i=0; i<urls.length; i++) {
 
-			
-				var request = new XMLHttpRequest();
-				request.onreadystatechange = function () {
-					var DONE = this.DONE || 4;
-					if (this.readyState === DONE){
-						var binStr = this.responseText;
-						var total = binStr.iTotalRecords;
-						while(start*20
-						console.log(binStr);
-						$("#doneMsg").val(binStr);
+			var total = 0;
+			var ret = null;
+			for(var i=0; i<urls.length; i++) {
+				MyAjax(urls[i], 'GET',  function(data){
+					date = new Date();
+					start = 0;
+					ret = JSON.parse(data); 
+					total = ret.iTotalRecords;
+					var uri = "";
+					while(start*20<total) {
+						switch (i) {
+							case 0:
+								uri= 'http://crm.meierbei.com/Customer/CustomerManageList?sEcho=3&iColumns=10&sColumns=&iDisplayStart='+start+'&iDisplayLength=20&mDataProp_0=0&mDataProp_1=1&mDataProp_2=2&mDataProp_3=3&mDataProp_4=4&mDataProp_5=5&mDataProp_6=6&mDataProp_7=7&mDataProp_8=8&mDataProp_9=9&searchKey={"cName":"","QQ":"","tel":"","Wechat":"","aTime_Start":"","aTime_End":"","intentionLevel":"","orderby":"desc","kefu":"107","tgtype":"","parea":"","carea":"","projectbig":"","LastGenjTime":"","extSearchKey":"QpCB2iuY4XU=","isFor":"0","AssClientId":"","AssClientName":"","ClientProvinceAreaId":"","ClientCityAreaId":"","isRecommend":false,"TagId":"0"}&_='+date.getTime();
+								break;
+							case 1:
+								uri = 'http://crm.meierbei.com/Customer/CustomerManageList?sEcho=2&iColumns=10&sColumns=&iDisplayStart='+start+'&iDisplayLength=20&mDataProp_0=0&mDataProp_1=1&mDataProp_2=2&mDataProp_3=3&mDataProp_4=4&mDataProp_5=5&mDataProp_6=6&mDataProp_7=7&mDataProp_8=8&mDataProp_9=9&searchKey={"cName":"","QQ":"","tel":"","Wechat":"","aTime_Start":"","aTime_End":"","intentionLevel":"","orderby":"desc","kefu":"107","tgtype":"","parea":"","carea":"","projectbig":"","LastGenjTime":"","extSearchKey":"bu7KP7O6V3w=","isFor":"0","AssClientId":"","AssClientName":"","ClientProvinceAreaId":"","ClientCityAreaId":"","isRecommend":false,"TagId":"0"}&_='+date.getTime();
+								break;
+							case 2:
+								uri = 'http://crm.meierbei.com/Customer/CustomerManageList?sEcho=2&iColumns=10&sColumns=&iDisplayStart='+start+'&iDisplayLength=20&mDataProp_0=0&mDataProp_1=1&mDataProp_2=2&mDataProp_3=3&mDataProp_4=4&mDataProp_5=5&mDataProp_6=6&mDataProp_7=7&mDataProp_8=8&mDataProp_9=9&searchKey={"cName":"","QQ":"","tel":"","Wechat":"","aTime_Start":"","aTime_End":"","intentionLevel":"","orderby":"desc","kefu":"107","tgtype":"","parea":"","carea":"","projectbig":"","LastGenjTime":"","extSearchKey":"vWAk1RlyioI=","isFor":"0","AssClientId":"","AssClientName":"","ClientProvinceAreaId":"","ClientCityAreaId":"","isRecommend":false,"TagId":"0"}&_='+date.getTime();
+								break;
+							case 3:	 
+								uri = 'http://crm.meierbei.com/Customer/CustomerManageList?sEcho=2&iColumns=10&sColumns=&iDisplayStart='+start+'&iDisplayLength=20&mDataProp_0=0&mDataProp_1=1&mDataProp_2=2&mDataProp_3=3&mDataProp_4=4&mDataProp_5=5&mDataProp_6=6&mDataProp_7=7&mDataProp_8=8&mDataProp_9=9&searchKey={"cName":"","QQ":"","tel":"","Wechat":"","aTime_Start":"","aTime_End":"","intentionLevel":"","orderby":"desc","kefu":"107","tgtype":"","parea":"","carea":"","projectbig":"","LastGenjTime":"","extSearchKey":"EhjPCKLe2b8=","isFor":"0","AssClientId":"","AssClientName":"","ClientProvinceAreaId":"","ClientCityAreaId":"","isRecommend":false,"TagId":"0"}&_='+date.getTime();
+								break;
+						}														
+						MyAjax(uri, 'GET', function(responseData){
+							var a = JSON.parse(responseData); 
+							var client = a.aaData;
+							for(var i=0; i<client.length; i++) {
+								var id = client[i].ID;
+								var name = client[i].RealName;
+								var content = $("#doneMsg").val();
+
+								var se = document.getElementById("content");
+								var optionElements = se.getElementsByTagName("option");
+								var len = optionElements.length;
+								var rad = parseInt(Math.random()*len);
+								var replyMsg = optionElements[rad].value;
+								
+								var retMsg = genJin(id, replyMsg);
+
+								content += id + ":" + name + "=>" + retMsg + "\n";
+								$("#doneMsg").val(content);
+							}			
+						});
+						start += 1;
 					}
-				};
-				request.open('GET', urls[i], true);
-				request.setRequestHeader('X-Requested-With', 'XMLHttpRequest'); 
-				request.send(null);
+
+				});
+
 			}
 		});
 	});
